e603d4c8b2e41a8940a5485d9a96d72bffbeb473
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-04-13 01:05:49 -0400

    ajustement

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@
 <?php wp_nav_menu(array(
         "menu"=> "menu_accueil",
         "container" => "nav")); ?>
-    <h1>---- Front-page.php ------</h1>
+    <h1>Front-page</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
