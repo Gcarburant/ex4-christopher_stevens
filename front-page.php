f2d23b629e8ced1b028fae7f4e606d6b3eee58be
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-04-12 21:55:04 -0400

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php get_header() ?>
+<main class="site__main">
+
+<section class="animation">
+    <div class="animation__bloc">1</div>
+    <div class="animation__bloc">2</div>
+    <div class="animation__bloc">3</div>
+    <div class="animation__bloc">4</div>
+    <div class="animation__bloc">5</div>
+</section>
+<?php wp_nav_menu(array(
+        "menu"=> "menu_accueil",
+        "container" => "nav")); ?>
+    <h1>---- Front-page.php ------</h1>
+   <?php if (have_posts()): the_post(); ?>
+        <?php the_title() ?>
+        <?php the_content() ?>   
+  
+   <?php endif ?>
+   
+</main>
+<?php get_footer() ?>
---
+++ readme.md
@@ -0,0 +1,2 @@
+# ex4-christopher_stevens
+ ex 4 pour 4w4
