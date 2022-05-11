486331b265f0021f6d74c43a70b1f828aa53b908
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-05-11 17:38:33 -0400

    fin cour de production
    just du clean up du code ,rien de major de changer

--- functions.php
+++ functions.php
@@ -5,10 +5,10 @@ require_once("option/apparence.php");
 function cidw_4w4_enqueue(){
     // wp_enqueue_style('style_css', get_stylesheet_uri());
     wp_enqueue_style('cidw-4w4-le-style', 
-                        get_template_directory_uri() . '/style.css', 
-                        array(), 
-                        filemtime(get_template_directory() . '/style.css'), 
-                        false);
+                    get_template_directory_uri() . '/style.css', 
+                    array(), 
+                    filemtime(get_template_directory() . '/style.css'), 
+                    false);
 
     wp_enqueue_style('cidw-4w4-police-google',
                     "https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins:wght@300;400;500&family=Roboto&display=swap");
@@ -20,10 +20,10 @@ function cidw_4w4_enqueue(){
                     true);                
 
     wp_register_script('cidw-4w4-carrousel', 
-                        get_template_directory_uri() . '/js/carrousel.js',
-                        array(),
-                        filemtime(get_template_directory() . '/js/carrousel.js'),
-                        true);   
+                    get_template_directory_uri() . '/js/carrousel.js',
+                    array(),
+                    filemtime(get_template_directory() . '/js/carrousel.js'),
+                    true);   
 
     if (is_category('cours')){
     wp_enqueue_script('cidw-4w4-boite-modale');                   
