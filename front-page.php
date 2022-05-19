1d9025d444df69348d323a4e2c763aa79c911803
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-05-19 14:44:04 -0400

    ef -cs (ajout menu)
    ajout d'un autre menu dans template atelier

--- front-page.php
+++ front-page.php
@@ -23,6 +23,10 @@
         "menu"=> "atelier",
         "container" => "nav")); ?> 
 
+        <?php wp_nav_menu(array(
+        "menu"=> "atelier",
+        "container" => "nav")); ?>
+
    <?php if (have_posts()): the_post(); ?>
         <h2><?php the_title() ?></h2>
         <?php the_content() ?>   
