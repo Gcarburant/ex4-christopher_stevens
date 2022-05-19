5872034dc72592f8cc6454eefc1c22823564bac6
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-05-19 14:49:13 -0400

    ef - cs (elenver l'autre menu)
    elenver l'autre menu car pas de besoin

--- front-page.php
+++ front-page.php
@@ -23,10 +23,6 @@
         "menu"=> "atelier",
         "container" => "nav")); ?> 
 
-        <?php wp_nav_menu(array(
-        "menu"=> "atelier",
-        "container" => "nav")); ?>
-
    <?php if (have_posts()): the_post(); ?>
         <h2><?php the_title() ?></h2>
         <?php the_content() ?>   
